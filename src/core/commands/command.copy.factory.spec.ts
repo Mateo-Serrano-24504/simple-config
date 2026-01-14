0c643d4e82fa1a2886a5974bc6e961b05684024a
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-01-14 23:40:05 +0000

    test:
    Added a test for the CommandCopyFactory class

---
+++ src/core/commands/command.copy.factory.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest'
+import { CommandCopyFactory } from './command.copy.factory'
+
+describe('CommandCopyFactory', () => {
+  it('should create a valid copy command', () => {
+    const factory = new CommandCopyFactory();
+    const result = factory.create('ran', 'dom');
+    expect(result.getSource()).toEqual('ran');
+    expect(result.getDestination()).toEqual('dom');
+  });
+})
