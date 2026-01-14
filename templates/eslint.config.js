cba913be6dc624b752906fa9aeda8f6aa267e2e3
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-01-14 17:26:57 +0000

    fix:
    Fixed the config file for eslint to correctly lint ts files

--- templates/eslint.config.js
+++ templates/eslint.config.js
@@ -5,12 +5,14 @@ export default [
     files: ['**/*.js', '**/*.ts'],
     languageOptions: {
       ecmaVersion: 'latest',
-      sourceType: 'module',
+      sourceType: 'commonjs',
       globals: {
         ...globals.node,
       },
     },
     rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
       'no-console': 'off',
     },
   },
