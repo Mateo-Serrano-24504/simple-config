2fe7c9bc3f57dc2167d929c7400e4f99a9e3d0f1
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-01-16 18:51:42 +0000

    chore:
    Added a file of classes instances

---
+++ src/instances.ts
@@ -0,0 +1,16 @@
+import {
+  Logger,
+  PathManager,
+  FileHandler,
+  FileCopier,
+  CommandInstallFactory,
+  PackageManager
+} from './core'
+
+export const logger = new Logger();
+export const pathManager = new PathManager();
+export const fileHandler = new FileHandler();
+export const fileCopier = new FileCopier(fileHandler, logger);
+export const commandInstallFactory = new CommandInstallFactory();
+export const packageManager = new PackageManager(fileHandler, pathManager);
+export const cwd = process.cwd();
