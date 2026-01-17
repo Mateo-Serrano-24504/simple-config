f7de57a20e4c08050f6bf6cd7e6653c8dde05490
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-01-17 19:37:09 +0000

    build:
    Removed the plugin for imports from the eslint config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import importPlugin from 'eslint-plugin-import'
 
 export default [
   {
@@ -8,26 +7,17 @@ export default [
     languageOptions: {
       parser: tseslint.parser,
       ecmaVersion: 'latest',
-      sourceType: 'commonjs',
+      sourceType: 'module',
       globals: {
         ...globals.node,
       },
     },
     plugins: {
       '@typescript-eslint': tseslint.plugin,
-      import: importPlugin,
     },
     rules: {
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
-      'import/extensions': [
-        'error',
-        'ignorePackages',
-        {
-          js: 'always',
-          ts: 'never',
-        },
-      ],
       'no-console': 'off',
     },
   },
